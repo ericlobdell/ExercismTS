9265db737b046cbe9636ed1ebfe1c94d841bae5f
Eric Lobdell <eric.lobdell@outlook.com>
2016-08-24 07:36:05 -0400

    finished grade school

---
+++ ExerecismTS/src/gradeschool.ts
@@ -0,0 +1,24 @@
+﻿
+class School {
+
+    constructor(private db = {}) { }
+
+    add( student: string, grade : number ) {
+
+        if (this.db[grade])
+            this.db[grade].push(student);
+        else
+            this.db[grade] = [student];
+
+        this.db[grade].sort();
+    }
+
+    grade( grade: number) {
+        return this.db[grade] ? this.db[grade] : [];
+    }
+
+    roster() {
+        return this.db;
+    }
+
+}
